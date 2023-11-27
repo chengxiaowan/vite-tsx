1d5f325f6d978891f05c4e82a0c7f834439e666a
drool <cheng_xiaowan@163.com>
2023-11-28 00:13:14 +0800

    修改一些细节

--- src/page/404/index.tsx
+++ src/page/404/index.tsx
@@ -1,8 +1,27 @@
-import { defineComponent } from "vue";
+import { defineComponent, h } from "vue";
+import { ElEmpty, ElButton } from "element-plus";
+import { useRouter } from "@/hooks/common.hooks";
+import "./index.less";
 
 export default defineComponent({
-  name: "note found!",
+  name: "not found!",
   setup() {
-    return () => <div id="404">404</div>;
+    const { navigateTo } = useRouter();
+    const description: string = "页面走丢了，哭哭~";
+
+    return () => (
+      <div id="notFound">
+        <div class="main-box">
+          <ElEmpty description={description} />
+          <div class="backBtn">
+            {h(
+              ElButton,
+              { onClick: () => navigateTo("/"), type: "primary" },
+              () => "返回首页"
+            )}
+          </div>
+        </div>
+      </div>
+    );
   },
 });
